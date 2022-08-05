5963354869868646a8829a1b7c857e9c653ddeaf
Alexander David Frick <Alex313031@gmail.com>
2022-08-05 08:51:30 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ printf "\n" &&
 printf "${YEL}Copying Mercury source files over the Mozilla tree...\n" &&
 tput sgr0 &&
 
-cp -r -v .mozconfig $HOME/mozilla-unified/ &&
+cp -r -v mozconfig $HOME/mozilla-unified/ &&
 cp -r -v application.ini $HOME/mozilla-unified/ &&
 cp -r -v browser/. $HOME/mozilla-unified/ &&
 cp -r -v app/. $HOME/mozilla-unified/browser/app/ &&
