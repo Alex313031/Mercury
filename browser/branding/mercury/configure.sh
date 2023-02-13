40abc6d2d2352dcca8b391ea49229369cc052871
Alexander David Frick <Alex313031@gmail.com>
2023-02-12 19:33:34 -0600

    Add files via upload

---
+++ browser/branding/mercury/configure.sh
@@ -0,0 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+MOZ_APP_NAME=Mercury
+
+MOZ_APP_BASENAME="Mercury"
+
+MOZ_APP_DISPLAYNAME=Mercury
+
+MOZ_APP_UA_NAME=Mercury
+
+MOZ_APP_PROFILE=Mercury
+
+MOZ_APP_REMOTINGNAME=Mercury
+
+MOZ_APP_VENDOR=Alex313031
+
+MOZ_APP_CODENAME=hydrargyrum
+
+MOZ_DEVTOOLS=all
