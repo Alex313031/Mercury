a5597c2f16853472f83b0101ffe93d30c6ba29da
Alexander Frick <Alex313031@gmail.com>
2023-06-30 23:25:30 -0500

    scripts up

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Copyright (c) 2023 Alex313031 and Midzer.
+
+YEL='\033[1;33m' # Yellow
+CYA='\033[1;96m' # Cyan
+RED='\033[1;31m' # Red
+GRE='\033[1;32m' # Green
+c0='\033[0m' # Reset Text
+bold='\033[1m' # Bold Text
+underline='\033[4m' # Underline Text
+
+# Error handling
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "${RED}Failed $*"; }
+
+# --help
+displayHelp () {
+	printf "\n" &&
+	printf "${bold}${GRE}Script to run Mercury in dev mode.${c0}\n" &&
+	printf "\n"
+}
+case $1 in
+	--help) displayHelp; exit 0;;
+esac
+
+printf "\n" &&
+printf "${bold}${GRE}Script to run Mercury in dev mode.${c0}\n" &&
+printf "\n" &&
+tput sgr0 &&
+
+cd $HOME/mozilla-unified &&
+
+./mach run
