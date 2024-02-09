2fcd03ea19189e3b2fcc02e63a586aaa79547618
Alexander Frick <Alex313031@gmail.com>
2024-02-09 09:31:11 -0600

    fixed portables

--- portable/RUN.sh
+++ portable/RUN.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Copyright (c) 2024 Alex313031.
-
-YEL='\033[1;33m' # Yellow
-CYA='\033[1;96m' # Cyan
-RED='\033[1;31m' # Red
-GRE='\033[1;32m' # Green
-c0='\033[0m' # Reset Text
-bold='\033[1m' # Bold Text
-underline='\033[4m' # Underline Text
-
-# Error handling
-yell() { echo "$0: $*" >&2; }
-die() { yell "$*"; exit 111; }
-try() { "$@" || die "${RED}Failed $*"; }
-
 mkdir -p ./USER_DATA &&
 
 ./mercury/mercury --profile "${PWD}/USER_DATA"
